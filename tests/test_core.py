5ca983f5d9d78c089641077b6d1b3591775f6ce0
Justin Cohen <jcohen@curtispackaging.com>
2023-08-30 13:07:40 -0400

    change test config

--- tests/test_core.py
+++ tests/test_core.py
@@ -6,21 +6,12 @@ from singer_sdk.testing import get_tap_test_class
 
 from tap_progressopenedge.tap import TapProgressOpenEdge
 
-SAMPLE_CONFIG = {
-    'DRIVER':'DataDirect 7.1 OpenEdge Wire Protocol',
-    'HOST':'HOST',
-    'PORT':'0000',
-    'DB':'TestDB',
-    'UID':'username',
-    'PWD':'pass'
-}
 
 
 # Run standard built-in tap tests from the SDK:
 TestTapProgressOpenEdge = get_tap_test_class(
     tap_class=TapProgressOpenEdge,
-    config=SAMPLE_CONFIG,
+    config=".test_config",
 )
 
 
-# TODO: Create additional tests as appropriate for your tap.
