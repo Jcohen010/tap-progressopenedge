f0ee3402db047829be02d4717429be491b2ac18f
Justin Cohen <jcohen@curtispackaging.com>
2023-08-30 13:13:26 -0400

    change test config

--- tests/test_core.py
+++ tests/test_core.py
@@ -6,12 +6,19 @@ from singer_sdk.testing import get_tap_test_class
 
 from tap_progressopenedge.tap import TapProgressOpenEdge
 
-
+sample_config = {
+    'Driver' : 'Test Driver',
+    'Host' : 'Test_Host',
+    'User' : 'Test_User',
+    'Password' : 'Pass',
+    'Database' : 'TestDB',
+    'DefaultSchema' : 'PUB'
+}
 
 # Run standard built-in tap tests from the SDK:
 TestTapProgressOpenEdge = get_tap_test_class(
     tap_class=TapProgressOpenEdge,
-    config=".test_config",
+    config=sample_config,
 )
 
 
